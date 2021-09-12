2327f3e288d3f29235c32a5b35c18a61f089e59f
Anh Nguyen <tuananh.exp@gmail.com>
2021-09-13 00:08:10 +0700

    ✨ Implement gift with purchase promotions

--- assets/cart.js
+++ assets/cart.js
@@ -1,17 +1,22 @@
 class CartRemoveButton extends HTMLElement {
   constructor() {
-    super();
+    super()
     this.addEventListener("click", (event) => {
-      event.preventDefault();
+      event.preventDefault()
       const shouldTriggerPromotionHandler = this.dataset.promotionItem !== 'true'
-      this.closest("cart-items").updateQuantity(this.dataset.index, 0, undefined, shouldTriggerPromotionHandler);
-    });
+      this.closest("cart-items").updateQuantity({
+        line: this.dataset.index,
+        quantity: 0,
+        shouldTriggerPromotionHandler,
+      })
+    })
   }
 }
 
-customElements.define("cart-remove-button", CartRemoveButton);
+customElements.define("cart-remove-button", CartRemoveButton)
 
-const GQL_URL = "https://gql.anhn-proxy.workers.dev/wavecommerce";
+const GQL_URL = "https://gql.anhn-proxy.workers.dev/wavecommerce"
+const SHOPIFY_PRICE_INDICATOR = 100
 const getCreateProductMutation = (data, price) => {
   return `
     mutation ProductCreateMutation {
@@ -25,8 +30,8 @@ const getCreateProductMutation = (data, price) => {
           },
           tags: "gift-product-created",
           variants: {
-            price: ${price / 100},
-            compareAtPrice: ${data.price / 100}
+            price: ${price / SHOPIFY_PRICE_INDICATOR},
+            compareAtPrice: ${data.price / SHOPIFY_PRICE_INDICATOR}
           },
           status: ACTIVE,
           published: true,
@@ -48,105 +53,172 @@ const getCreateProductMutation = (data, price) => {
     }
   `
 }
+class PromotionServices {
+  constructor() {
+    this.promotions = window?.promotions || []
+  }
+
+  getPromotions() {
+    return this.promotions
+  }
+
+  async getCartData() {
+    try {
+      const response = await fetch('/cart.js', {
+        method: 'GET'
+      })
+      const data = await response.json()
+  
+      return data
+    } catch (error) {
+      console.error(error)
+
+      return undefined
+    }
+  }
+
+  getPromotionInfoFromCart(cartData, promotion) {
+    let giftProduct = undefined
+    let spentAmount = 0
+    let giftProductInCart = undefined
+
+    cartData?.items?.forEach?.((item, idx) => {
+      if (item?.id === promotion.productGiftId) {
+        giftProduct = {
+          ...item,
+          lineIndex: idx + 1,
+        }
+      }
+
+      if (item?.properties?._id === promotion.id) {
+        giftProductInCart = {
+          ...item,
+          lineIndex: idx + 1,
+        }
+      }
+
+      if (promotion.productsAppliedPromotion.includes(item?.id)) {
+        const totalItemPrice = item?.price * item?.quantity
+        spentAmount += (totalItemPrice / SHOPIFY_PRICE_INDICATOR)
+      }
+    })
+
+    return {
+      giftProduct,
+      spentAmount,
+      giftProductInCart,
+    }
+  }
+
+  async createGiftProduct({ giftProduct, discountedPrice }) {
+    try {
+      const response = await fetch(GQL_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          query: getCreateProductMutation(giftProduct, discountedPrice),
+        }),
+      })
+      const createdItem = await response.json()
+      const createdItemId = createdItem?.data?.productCreate?.product?.variants?.edges?.[0]?.node?.id?.match?.(/([^\/]+)$/ig)?.[0]
+
+      return createdItemId
+    } catch (error) {
+      console.error(error)
+
+      return undefined
+    }
+  }
+}
 
 class CartItems extends HTMLElement {
   constructor() {
-    super();
+    super()
 
     this.lineItemStatusElement = document.getElementById(
       "shopping-cart-line-item-status"
-    );
+    )
 
     this.currentItemCount = Array.from(
       this.querySelectorAll('[name="updates[]"]')
     ).reduce(
       (total, quantityInput) => total + parseInt(quantityInput.value),
       0
-    );
+    )
 
     this.debouncedOnChange = debounce((event) => {
-      this.onChange(event);
-    }, 300);
+      this.onChange(event)
+    }, 300)
 
-    this.addEventListener("change", this.debouncedOnChange.bind(this));
+    this.addEventListener("change", this.debouncedOnChange.bind(this))
     
-    this.promotionHandler();
+    this.promotionHandler()
   }
 
   onChange(event) {
-    this.updateQuantity(
-      event.target.dataset.index,
-      event.target.value,
-      document.activeElement.getAttribute("name")
-    );
+    this.updateQuantity({
+      line: event.target.dataset.index,
+      quantity: event.target.value,
+      name: document.activeElement.getAttribute("name"),
+    })
   }
 
   async promotionHandler() {
-    const promotions = window?.promotions || []
+    const promotionServices = new PromotionServices()
+    const promotions = promotionServices.getPromotions()
     if (promotions.length <= 0) return
 
     this.enableWholeCartLoading()
-    const cartResponse = await fetch('/cart.js', {
-      method: 'GET'
-    })
-    const cartData = await cartResponse.json()
+    const cartData = await promotionServices.getCartData()
 
     for (let i = 0; i <= promotions.length - 1; i++) {
       const promotion = promotions[i]
       const isGiftItemInThePool = promotion.productsAppliedPromotion.includes(promotion.productGiftId)
-      let giftItemFullInfo = undefined
-      let itemsInThePoolTotalAmount = 0
-      let promotionAppliedItem = undefined
+      const {
+        giftProduct,
+        spentAmount,
+        giftProductInCart,
+      } = promotionServices.getPromotionInfoFromCart(cartData, promotion)
 
-      cartData.items.forEach((item, idx) => {
-        if (item?.properties?._ === promotion.id) {
-          promotionAppliedItem = {
-            ...item,
-            lineIndex: idx + 1,
-          }
-        }
+      if (!giftProductInCart && giftProduct) {
+        const promotionDiscountValue = (giftProduct.price * promotion.discountValue) / 100
+        const discountedPrice = giftProduct.price - promotionDiscountValue
+        const totalAmountAfterDiscountApplied = isGiftItemInThePool ? (spentAmount - (promotionDiscountValue / SHOPIFY_PRICE_INDICATOR)) : spentAmount
 
-        if (item.id === promotion.productGiftId) {
-          giftItemFullInfo = {
-            ...item,
-            lineIndex: idx + 1,
-          }
-        }
-
-        if (promotion.productsAppliedPromotion.includes(item.id)) {
-          itemsInThePoolTotalAmount += (item.price * item.quantity / 100)
-        }
-      })
-
-      if (!promotionAppliedItem && giftItemFullInfo) {
-        const promotionDiscountValue = (giftItemFullInfo.price * promotion.discountValue) / 100
-        const discountedPrice = giftItemFullInfo.price - promotionDiscountValue
-        const totalAmountAfterDiscount = isGiftItemInThePool ? (itemsInThePoolTotalAmount - (promotionDiscountValue / 100)) : itemsInThePoolTotalAmount
-
-        if (totalAmountAfterDiscount >= promotion.spendAmount) {
-          const createdItemResponse = await fetch(GQL_URL, {
-            method: "POST",
-            body: JSON.stringify({
-              query: getCreateProductMutation(giftItemFullInfo, discountedPrice),
-            }),
-          });
-          const createdItemData = await createdItemResponse.json()
-          const createdItemId = createdItemData.data.productCreate.product.variants.edges[0].node.id.match(/([^\/]+)$/ig)[0]
-
-          await this.addItem(createdItemId, 1, {
-            discount: promotion.name,
-            _originalPrice: giftItemFullInfo.price,
-            _: promotion.id,
+        // Handle cart eligibility
+        if (totalAmountAfterDiscountApplied >= promotion.spendAmount) {
+          const createdItemId = await promotionServices.createGiftProduct({
+            giftProduct,
+            discountedPrice,
           })
-          const newQuantity = cartData.items[giftItemFullInfo.lineIndex - 1].quantity - 1
-          await this.updateQuantityByKey(giftItemFullInfo.key, newQuantity)
 
-          cartData.items[giftItemFullInfo.lineIndex - 1].quantity = newQuantity
+          await this.addItem({
+            id: createdItemId,
+            quantity: 1,
+            properties: {
+              discount: promotion.name,
+              _originalPrice: giftProduct.price,
+              _id: promotion.id,
+            }
+          })
+
+          // Update gift product 
+          const giftProductIndex = giftProduct.lineIndex - 1
+          const newQuantity = cartData?.items?.[giftProductIndex]?.quantity - 1
+          await this.updateQuantity({
+            id: giftProduct.key,
+            quantity: newQuantity,
+            shouldTriggerPromotionHandler: false
+          })
+          cartData.items[giftProductIndex].quantity = newQuantity
         }
       }
 
-      if (promotionAppliedItem && itemsInThePoolTotalAmount < promotion.spendAmount) {
-        this.updateQuantity(promotionAppliedItem.lineIndex, 0)
+      // Remove gift product if is in cart and promotion conditions are not matched
+      if (giftProductInCart && spentAmount < promotion.spendAmount) {
+        this.updateQuantity({
+          line: giftProductInCart.lineIndex,
+          quantity: 0,
+        })
       }
     }
 
@@ -175,140 +247,102 @@ class CartItems extends HTMLElement {
         section: document.getElementById("main-cart-footer").dataset.id,
         selector: ".js-contents",
       },
-    ];
+    ]
   }
 
-  async updateQuantity(line, quantity, name, shouldTriggerPromotionHandler = true) {
-    this.enableWholeCartLoading();
-
-    const body = JSON.stringify({
-      line,
-      quantity,
-      sections: this.getSectionsToRender().map((section) => section.section),
-      sections_url: window.location.pathname,
-    });
+  async requestCartApi({
+    url,
+    requestData,
+    showLoadingIndicator,
+    line,
+    name,
+  }) {
+    if (showLoadingIndicator) this.enableWholeCartLoading()
 
     try {
-      const res = await fetch(`${routes.cart_change_url}`, { ...fetchConfig(), ...{ body } })
+      const res = await fetch(url, { ...fetchConfig(), body: requestData })
       const state = await res.text()
-      const parsedState = JSON.parse(state);
-        this.classList.toggle("is-empty", parsedState.item_count === 0);
+      const parsedState = JSON.parse(state)
+        this.classList.toggle("is-empty", parsedState.item_count === 0)
         document
           .getElementById("main-cart-footer")
-          ?.classList.toggle("is-empty", parsedState.item_count === 0);
+          ?.classList.toggle("is-empty", parsedState.item_count === 0)
 
         this.getSectionsToRender().forEach((section) => {
           const elementToReplace =
             document
               .getElementById(section.id)
               .querySelector(section.selector) ||
-            document.getElementById(section.id);
+            document.getElementById(section.id)
 
           elementToReplace.innerHTML = this.getSectionInnerHTML(
             parsedState.sections[section.section],
             section.selector
-          );
-        });
+          )
+        })
 
-        this.updateLiveRegions(line, parsedState.item_count);
-        document
-          .getElementById(`CartItem-${line}`)
-          ?.querySelector(`[name="${name}"]`)
-          ?.focus();
-        this.disableWholeCartLoading();
-
-        if (shouldTriggerPromotionHandler) {
-          await this.promotionHandler()
+        if (line && name) {
+          this.updateLiveRegions(line, parsedState.item_count)
+          document
+            .getElementById(`CartItem-${line}`)
+            ?.querySelector(`[name="${name}"]`)
+            ?.focus()
         }
+        
+        this.disableWholeCartLoading()
     } catch (error) {
       this.querySelectorAll(".loading-overlay").forEach((overlay) =>
         overlay.classList.add("hidden")
-      );
+      )
       document.getElementById("cart-errors").textContent =
-        window.cartStrings.error;
-      this.disableWholeCartLoading();
+        window.cartStrings.error
+      this.disableWholeCartLoading()
     }
   }
 
-  async updateQuantityByKey(key, quantity) {
+  async updateQuantity({
+    id,
+    line,
+    quantity,
+    name,
+    shouldTriggerPromotionHandler = true,
+  }) {
     const body = JSON.stringify({
-      id: key,
+      ...(id ? { id } : { line }),
       quantity,
       sections: this.getSectionsToRender().map((section) => section.section),
       sections_url: window.location.pathname,
-    });
+    })
 
-    try {
-      const res = await fetch(`${routes.cart_change_url}`, { ...fetchConfig(), ...{ body } })
-      const state = await res.text()
+    await this.requestCartApi({
+      url: routes.cart_change_url,
+      requestData: body,
+      showLoadingIndicator: true,
+      line,
+      name
+    })
 
-      const parsedState = JSON.parse(state);
-      this.classList.toggle("is-empty", parsedState.item_count === 0);
-      document
-        .getElementById("main-cart-footer")
-        ?.classList.toggle("is-empty", parsedState.item_count === 0);
-
-      this.getSectionsToRender().forEach((section) => {
-        const elementToReplace =
-          document
-            .getElementById(section.id)
-            .querySelector(section.selector) ||
-          document.getElementById(section.id);
-
-        elementToReplace.innerHTML = this.getSectionInnerHTML(
-          parsedState.sections[section.section],
-          section.selector
-        );
-      });
-    } catch (error) {
-      this.querySelectorAll(".loading-overlay").forEach((overlay) =>
-        overlay.classList.add("hidden")
-      );
-      document.getElementById("cart-errors").textContent =
-        window.cartStrings.error;
+    if (shouldTriggerPromotionHandler) {
+      await this.promotionHandler()
     }
   }
 
-  async addItem(id, quantity, props, callback) {
+  async addItem({
+    id, quantity, properties,
+  }) {
     const body = JSON.stringify({
       id,
       quantity,
+      properties,
       sections: this.getSectionsToRender().map((section) => section.section),
       sections_url: window.location.pathname,
-      properties: props,
-    });
+    })
 
-    try {
-      const res = await fetch("/cart/add.js", { ...fetchConfig(), ...{ body } })
-      const state = await res.text()
-
-      const parsedState = JSON.parse(state);
-        this.classList.toggle("is-empty", parsedState.item_count === 0);
-        document
-          .getElementById("main-cart-footer")
-          ?.classList.toggle("is-empty", parsedState.item_count === 0);
-
-        this.getSectionsToRender().forEach((section) => {
-          const elementToReplace =
-            document
-              .getElementById(section.id)
-              .querySelector(section.selector) ||
-            document.getElementById(section.id);
-
-          elementToReplace.innerHTML = this.getSectionInnerHTML(
-            parsedState.sections[section.section],
-            section.selector
-          );
-        });
-
-        callback?.()
-    } catch (err) {
-      this.querySelectorAll(".loading-overlay").forEach((overlay) =>
-        overlay.classList.add("hidden")
-      );
-      document.getElementById("cart-errors").textContent =
-        window.cartStrings.error;
-    }
+    await this.requestCartApi({
+      url: '/cart/add.js',
+      requestData: body,
+      showLoadingIndicator: false,
+    })
   }
 
   updateLiveRegions(line, itemCount) {
@@ -319,61 +353,44 @@ class CartItems extends HTMLElement {
         window.cartStrings.quantityError.replace(
           "[quantity]",
           document.getElementById(`Quantity-${line}`).value
-        );
+        )
     }
 
-    this.currentItemCount = itemCount;
-    this.lineItemStatusElement.setAttribute("aria-hidden", true);
+    this.currentItemCount = itemCount
+    this.lineItemStatusElement.setAttribute("aria-hidden", true)
 
-    const cartStatus = document.getElementById("cart-live-region-text");
-    cartStatus.setAttribute("aria-hidden", false);
+    const cartStatus = document.getElementById("cart-live-region-text")
+    cartStatus.setAttribute("aria-hidden", false)
 
     setTimeout(() => {
-      cartStatus.setAttribute("aria-hidden", true);
-    }, 1000);
+      cartStatus.setAttribute("aria-hidden", true)
+    }, 1000)
   }
 
   getSectionInnerHTML(html, selector) {
     return new DOMParser()
       .parseFromString(html, "text/html")
-      .querySelector(selector).innerHTML;
-  }
-
-  enableLoading(line) {
-    document
-      .getElementById("main-cart-items")
-      .classList.add("cart__items--disabled");
-    this.querySelectorAll(".loading-overlay")[line - 1].classList.remove(
-      "hidden"
-    );
-    document.activeElement.blur();
-    this.lineItemStatusElement.setAttribute("aria-hidden", false);
-  }
-
-  disableLoading() {
-    document
-      .getElementById("main-cart-items")
-      .classList.remove("cart__items--disabled");
+      .querySelector(selector).innerHTML
   }
 
   enableWholeCartLoading() {
     document
       .getElementById("main-cart-items")
-      ?.classList?.add("cart__items--disabled");
+      ?.classList?.add("cart__items--disabled")
     this.querySelectorAll(".cart-loading-overlay")?.[0]?.classList?.remove(
       "hidden"
-    );
+    )
   }
 
   disableWholeCartLoading() {
     document
       .getElementById("main-cart-items")
-      ?.classList.remove("cart__items--disabled");
+      ?.classList.remove("cart__items--disabled")
 
     this.querySelectorAll(".cart-loading-overlay")?.[0]?.classList?.add(
       "hidden"
-    );
+    )
   }
 }
 
-customElements.define("cart-items", CartItems);
+customElements.define("cart-items", CartItems)
